5aebacdcec7cd84ff07126028144d0e6685fefe3
giftdavid101 <giftdavid101@gmail.com>
2024-08-15 04:15:31 +0100

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,9 @@
+#debug a server
+$file_path = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_path}",
+  path    => ['/bin','/usr/bin']
+}
